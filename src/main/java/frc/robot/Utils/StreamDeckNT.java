679ea6c45af3388c62a768e01cb2251ef3637ce2
Nobara_Kugisaki#1120 <esthermacieira1@gmail.com>
2025-12-24 22:30:00 -0300

    modificações teste

--- src/main/java/frc/robot/Utils/StreamDeckNT.java
+++ src/main/java/frc/robot/Utils/StreamDeckNT.java
@@ -5,22 +5,60 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 public class StreamDeckNT {
 
-  private final GenericEntry b1;
-  private final GenericEntry b2;
-  private final GenericEntry b3;
-  private final GenericEntry b4;
+  private final GenericEntry[] buttons = new GenericEntry[15];
 
   public StreamDeckNT() {
     var tab = Shuffleboard.getTab("StreamDeck");
 
-    b1 = tab.add("Button 1", false).getEntry();
-    b2 = tab.add("Button 2", false).getEntry();
-    b3 = tab.add("Button 3", false).getEntry();
-    b4 = tab.add("Button 4", false).getEntry();
+    for (int i = 0; i < 15; i++) {
+      buttons[i] = tab.add("Button " + (i + 1), false).getEntry();
+    }
   }
 
-  public boolean b1() { return b1.getBoolean(false); }
-  public boolean b2() { return b2.getBoolean(false); }
-  public boolean b3() { return b3.getBoolean(false); }
-  public boolean b4() { return b4.getBoolean(false); }
+  public boolean button1Pressed()  { 
+    return buttons[0].getBoolean(false); 
 }
+  public boolean button2Pressed()  { 
+    return buttons[1].getBoolean(false); 
+}
+  public boolean button3Pressed()  { 
+    return buttons[2].getBoolean(false); 
+}
+  public boolean button4Pressed()  { 
+    return buttons[3].getBoolean(false); 
+}
+  public boolean button5Pressed()  { 
+    return buttons[4].getBoolean(false); 
+}
+  public boolean button6Pressed()  { 
+    return buttons[5].getBoolean(false); 
+}
+  public boolean button7Pressed()  { 
+    return buttons[6].getBoolean(false); 
+}
+  public boolean button8Pressed()  { 
+    return buttons[7].getBoolean(false); 
+}
+  public boolean button9Pressed()  { 
+    return buttons[8].getBoolean(false); 
+}
+  public boolean button10Pressed() { 
+    return buttons[9].getBoolean(false); 
+}
+  public boolean button11Pressed() { 
+    return buttons[10].getBoolean(false); 
+}
+  public boolean button12Pressed() { 
+    return buttons[11].getBoolean(false); 
+}
+  public boolean button13Pressed() { 
+    return buttons[12].getBoolean(false);
+ }
+  public boolean button14Pressed() { 
+    return buttons[13].getBoolean(false); 
+}
+  public boolean button15Pressed() { 
+    return buttons[14].getBoolean(false); 
+}
+}
+//           "Swerve/Module" + i + "/AngleDeg",
