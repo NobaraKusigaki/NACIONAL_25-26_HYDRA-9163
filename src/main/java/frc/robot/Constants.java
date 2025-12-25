93318b3fbcba189853a7f9b2daa57f541ab78da2
Nobara_Kugisaki#1120 <esthermacieira1@gmail.com>
2025-12-24 22:09:17 -0300

    primeiro

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,25 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+
+public final class Constants {
+
+  public static final double ROBOT_MASS = 51.25 * 0.453592;
+  public static final Matter CHASSIS =
+      new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+  public static final double LOOP_TIME = 0.13;
+  public static final double MAX_SPEED = Units.feetToMeters(12);
+
+  public static final class DrivebaseConstants {
+    public static final double WHEEL_LOCK_TIME = 10;
+  }
+
+  public static class OperatorConstants {
+    public static final double DEADBAND = 0.05;
+    public static final double LEFT_Y_DEADBAND = 0.05;
+    public static final double RIGHT_X_DEADBAND = 0.05;
+    public static final double TURN_CONSTANT = 2;
+  }
+}
