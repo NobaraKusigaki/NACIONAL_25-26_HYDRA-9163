91b7663f0657966cf9d62670740cd4bfda6fed0e
Esther <esthermacieira1@gmail.com>
2026-01-07 15:22:28 -0300

    dashboard de stress completa

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,25 +1,13 @@
 package frc.robot;
 
-import com.pathplanner.lib.auto.NamedCommands;
-
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.RobotController;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+
 import frc.robot.Constants.OperatorConstants;
+import frc.robot.dashboards.*;
 import frc.robot.subsystems.Swervedrive.SwerveSubsystem;
-import frc.robot.Utils.StreamDeckNT;
-import frc.robot.dashboards.RobotStressMonitor;
-import frc.robot.dashboards.DashboardPublisher;
-import frc.robot.dashboards.RobotStressController;
-import frc.robot.dashboards.RobotStressData;
-import edu.wpi.first.networktables.NetworkTableInstance;
 
 import java.io.File;
 
@@ -28,92 +16,71 @@ import swervelib.SwerveInputStream;
 public class RobotContainer {
 
   private final CommandPS5Controller controller = new CommandPS5Controller(0);
-  @SuppressWarnings("unused")
-  private final StreamDeckNT streamDeck = new StreamDeckNT();
-  private final RobotStressController stressController = new RobotStressController();
-  private double driveSpeedScale = 1.0;
 
   private final SwerveSubsystem drivebase =
       new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve/neo"));
 
-  private final RobotStressMonitor stressMonitor = new RobotStressMonitor(30); 
+  private final RobotStressMonitor stressMonitor = new RobotStressMonitor();
+  private final RobotStressController stressController = new RobotStressController();
   private final DashboardPublisher dashboardPublisher = new DashboardPublisher();
 
-  private final int[] drivetrainPDHChannels = {0, 2, 3, 6, 10, 14, 15, 19};
+  private double driveSpeedScale = 1.0;
 
-  SwerveInputStream driveAngularVelocity =
-    SwerveInputStream.of(
-        drivebase.getSwerveDrive(),
-        () -> -controller.getLeftY(),
-        () -> -controller.getLeftX()
-    )
-    .withControllerRotationAxis(() -> controller.getRightX())
-    .deadband(OperatorConstants.DEADBAND)
-    .scaleTranslation(driveSpeedScale)
-    .allianceRelativeControl(true);
-
-
-  SwerveInputStream driveDirectAngle =
-      driveAngularVelocity
-          .copy()
-          .withControllerHeadingAxis(controller::getRightX, controller::getRightY)
-          .headingWhile(true);
+  private final SwerveInputStream driveAngularVelocity =
+      SwerveInputStream.of(
+              drivebase.getSwerveDrive(),
+              () -> -controller.getLeftY(),
+              () -> -controller.getLeftX())
+          .withControllerRotationAxis(() -> controller.getRightX())
+          .deadband(OperatorConstants.DEADBAND)
+          .scaleTranslation(driveSpeedScale)
+          .allianceRelativeControl(true);
 
   public RobotContainer() {
     configureBindings();
-
     DriverStation.silenceJoystickConnectionWarning(true);
-    NamedCommands.registerCommand("test", Commands.print("I EXIST"));
   }
 
   private void configureBindings() {
-    Command driveFieldOrientedDirectAngle =
-        drivebase.driveFieldOriented(driveDirectAngle);
 
-    drivebase.setDefaultCommand(driveFieldOrientedDirectAngle);
-    // controller.cross().onTrue(Commands.runOnce(drivebase::zeroGyro));
-   
+    drivebase.setDefaultCommand(
+        drivebase.driveFieldOriented(driveAngularVelocity));
+
     controller.triangle().onTrue(
-    drivebase.snapToAngleOnce(
-        Rotation2d.fromDegrees(0),
-        () -> new Translation2d()));
+        drivebase.snapToAngleOnce(Rotation2d.fromDegrees(0)));
 
-      controller.circle().onTrue(
-      drivebase.snapToAngleOnce(
-          Rotation2d.fromDegrees(90),
-          () -> new Translation2d()));
+    controller.circle().onTrue(
+        drivebase.snapToAngleOnce(Rotation2d.fromDegrees(90)));
 
-       controller.cross().onTrue(
-       drivebase.snapToAngleOnce(
-         Rotation2d.fromDegrees(180),
-         () -> new Translation2d()));
+    controller.cross().onTrue(
+        drivebase.snapToAngleOnce(Rotation2d.fromDegrees(180)));
 
-         controller.square().onTrue(
-          drivebase.snapToAngleOnce(
-              Rotation2d.fromDegrees(270),
-              () -> new Translation2d()));   
-
+    controller.square().onTrue(
+        drivebase.snapToAngleOnce(Rotation2d.fromDegrees(270)));
   }
+
 
   public void updateDashboards() {
 
     RobotStressData stressData =
-        stressMonitor.generateData(drivetrainPDHChannels);
-        stressController.update(stressData);
+        stressMonitor.generateData(drivebase);
+
+    stressController.update(stressData);
 
     driveSpeedScale = stressController.getMaxAllowedSpeedMps() / Constants.MAX_SPEED;
-    double chassisSpeed =drivebase.getRobotVelocity().vxMetersPerSecond;
+
+    double chassisSpeed =
+        Math.abs(drivebase.getRobotVelocity().vxMetersPerSecond);
 
     dashboardPublisher.publish(
         stressData,
         driveSpeedScale,
-        Math.abs(chassisSpeed)
-    );
-}
+        chassisSpeed);
+  }
 
-public Command getAutonomousCommand() {
-  return drivebase.getAutonomousCommand("AutoSimple");
-}
+  public Command getAutonomousCommand() {
+    return drivebase.getAutonomousCommand("AutoSimple");
+  }
 
   public void setMotorBrake(boolean brake) {
     drivebase.setMotorBrake(brake);
