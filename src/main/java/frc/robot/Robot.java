9666804fc71d97563d6fbd253898f41f7c5c4e0c
raphacnas <raphacnas@gmail.com>
2026-01-11 12:54:35 -0300

    Mudanças gerais da dashboard da limelight. Falta apenas a integração com os dados de botôes da networkTables

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,13 +7,9 @@ package frc.robot;
 import org.littletonrobotics.junction.networktables.NT4Publisher;
 import org.littletonrobotics.junction.wpilog.WPILOGWriter;
 
-import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.Score.StreamDeckMotorController;
-
-import java.util.stream.Stream;
 
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
